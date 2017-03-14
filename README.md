f68eda0c0bb4168c054a82e8986394e5bebc4fe9
naesheim <hg.nesheim@gmail.com>
2017-03-14 17:20:25 +0100

    initital commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Package Plugin
+
+This gradle plugin collects artifacts from one repo and promote them to a new repo as a bundled up zip.
+
+## How
+
+1. Add the gradle plugin to your build.gradle:
+```
+    plugins {
+        id "net.praqma.package" version "1.0.0"
+    }
+```
+
+2. Add upload folder to build.gradle
+```
+buildproperties.publishing {
+    zip {
+        from ('build/resolvedDep/')
+    }
+}
+```
+
+3. Update package.properties
+```
+    productName=ReleasePackage
+    version=1.0.1  
+    group=net.praqma
+    resolveRepo=libs-release-local
+    destinationRepo=promote-release-local
+    artifacts=artifacts.txt
+```
+
+4. Add your artifacts to a file (artifacts.txt) divided by new line, containing group:name:version
+```
+    com.gradle:plugin:1.2.3
+    net.default.customLib:1.0.0
+```
+5. Update gradle.properties in your ~/.gradle folder
+```
+repositoryManagerUsername=username
+repositoryManagerPassword=password
+repositoryManagerUrl=http://127.0.0.1:8081/repository
+```
+
+6. Promote your artifacts with
+```
+    OSX or LINUX: ./gradlew publish
+    WIN : gradlew publish
+```
---
+++ src/main/groovy/net/praqma/BuildPublishing.groovy
@@ -0,0 +1,42 @@
+package net.praqma
+
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Zip
+
+
+@CompileStatic
+class BuildPublishing {
+
+    private final BuildExtension extension
+
+    BuildPublishing(BuildExtension extension) {
+        this.extension = extension
+    }
+
+    @CompileDynamic
+    void zip(Closure closure) {
+        String pubName = 'publication'
+        Project project = extension.project
+
+        Task executeBuildTask = project.resolveDependencies
+
+        Task task = project.tasks.create(name: "moveDependencies", type: Zip) {
+            dependsOn executeBuildTask
+            with project.copySpec(closure)
+        }
+
+        project.publishing.publications {
+                "${pubName}"(MavenPublication) {
+                    groupId project.group
+                    version project.version
+                    artifactId extension.productName
+
+                    artifact task
+                }
+        }
+    }
+}
